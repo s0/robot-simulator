f3f539ab21fd2d6129542a408de32d26586e688f
Sam Lanning <sam@samlanning.com>
2015-05-04 22:02:03 +0100

    Keep Left Bot

--- src/com/samlanning/robot_simulator/robots/CustomRobot.java
+++ src/com/samlanning/robot_simulator/robots/CustomRobot.java
@@ -1,5 +1,6 @@
 package com.samlanning.robot_simulator.robots;
 
+import com.samlanning.robot_simulator.iface.MapBlock;
 import com.samlanning.robot_simulator.iface.Robot;
 import com.samlanning.robot_simulator.iface.RobotControl;
 
@@ -7,7 +8,17 @@ public class CustomRobot implements Robot {
 
     @Override
     public void run(final RobotControl control) {
-
+        
+        while(true){
+            
+            control.turnLeft();
+            while(control.lookAhead() == MapBlock.WALL)
+                control.turnRight();
+            
+            control.moveForward();
+            
+        }
+        
     }
     
 }
