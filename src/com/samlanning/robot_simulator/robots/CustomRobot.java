c4132447f2436ac55599147ee1c66e2964a2157c
Sam Lanning <sam@samlanning.com>
2015-05-10 14:07:22 +0100

    MyRobot -> CustomRobot

--- src/com/samlanning/robot_simulator/robots/CustomRobot.java
+++ src/com/samlanning/robot_simulator/robots/CustomRobot.java
@@ -3,7 +3,7 @@ package com.samlanning.robot_simulator.robots;
 import com.samlanning.robot_simulator.iface.Robot;
 import com.samlanning.robot_simulator.iface.RobotControl;
 
-public class MyRobot implements Robot {
+public class CustomRobot implements Robot {
 
     @Override
     public void run(final RobotControl control) {
--- src/com/samlanning/robot_simulator/robots/RobotsEnum.java
+++ src/com/samlanning/robot_simulator/robots/RobotsEnum.java
@@ -9,7 +9,7 @@ import com.samlanning.robot_simulator.iface.Robot;
  */
 public enum RobotsEnum {
     
-    MY_ROBOT(new MyRobot(), new Color(100, 100, 100)),
+    MY_ROBOT(new CustomRobot(), new Color(100, 100, 100)),
     RANDOM(new RandomRobot(), new Color(0, 100, 100)),
     ;
     
