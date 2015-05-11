71ef5475af9c70882b53a250e6c2dfe60108e070
Sam Lanning <sam@samlanning.com>
2015-05-11 21:54:37 +0100

    Activate system look and feel.

--- src/com/samlanning/robot_simulator/simulator/GUISimulator.java
+++ src/com/samlanning/robot_simulator/simulator/GUISimulator.java
@@ -1,10 +1,20 @@
 package com.samlanning.robot_simulator.simulator;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import com.samlanning.robot_simulator.simulator.gui.MainWindow;
 
 public class GUISimulator {
     
     public static void main(String[] args){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+            | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+            // Ignore
+        }
         new MainWindow().setVisible(true);;
     }
     
