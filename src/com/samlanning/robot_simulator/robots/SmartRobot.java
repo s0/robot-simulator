4da8a7270d5443f43afa051b3879daf0f94b5c44
Sam Lanning <sam@samlanning.com>
2015-05-11 23:29:46 +0100

    Renamed MyRobot to SmartRobot

--- src/com/samlanning/robot_simulator/robots/SmartRobot.java
+++ src/com/samlanning/robot_simulator/robots/SmartRobot.java
@@ -4,7 +4,7 @@ import com.samlanning.robot_simulator.iface.MapBlock;
 import com.samlanning.robot_simulator.iface.Robot;
 import com.samlanning.robot_simulator.iface.RobotControl;
 
-public class CustomRobot implements Robot {
+public class SmartRobot implements Robot {
 
     @Override
     public void run(final RobotControl control) {
